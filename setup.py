206d840d7388a498a6670fcf88ad4be8219bbc2e
evilbinary <rootntsd@gmail.com>
2024-05-26 10:38:51 +0800

    update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
     name="yymake",
-    version="0.8.6",
+    version="0.8.7",
     author="evilbinary",
     author_email="rootntsd@gmail.com",
     description="A cross build dsl make tool",
